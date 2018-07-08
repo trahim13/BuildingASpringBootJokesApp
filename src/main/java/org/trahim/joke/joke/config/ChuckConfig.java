1b97bb1ec7018adf238e1605325af79bd8ec7c02
trahim13 <trachimovich@gmail.com>
2018-07-08 22:38:02 +0300

    Java configuration example.

---
+++ src/main/java/org/trahim/joke/joke/config/ChuckConfig.java
@@ -0,0 +1,14 @@
+package org.trahim.joke.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfig {
+
+    @Bean
+    ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/org/trahim/joke/joke/service/JokeServiceImpl.java
+++ src/main/java/org/trahim/joke/joke/service/JokeServiceImpl.java
@@ -10,9 +10,8 @@ public class JokeServiceImpl implements JokeService {
     private ChuckNorrisQuotes chuckNorrisQuotes;
 
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
-
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
